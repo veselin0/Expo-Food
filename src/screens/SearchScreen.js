4e56eec9e5587fe86994b20f2e56004daf77f50d
Veselin <vesselin60@gmail.com>
2022-03-02 08:07:06 +0200

    configure navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+	return (
+		<View
+			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+		>
+			<Text>Search Screen</Text>
+		</View>
+	);
+}
+
+export default SearchScreen;
