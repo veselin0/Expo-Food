c9427a2c42926f29d0cc6c334c4d049faddd5506
Veselin <vesselin60@gmail.com>
2022-03-04 10:05:15 +0200

    attempting to solve cosr problem

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+
 export default axios.create({
-	baseURL: "https://api.yelp.com/v3/businesses",
+	baseURL: 'https://api.yelp.com/v3/businesses',
 	headers: {
 		Authorization:
 			"Bearer Su5rA-Yw7VLvsYJe5quuZzDkRkak24-kHUAMp7QlbZ_KZednICvIHQrUvhL8CuQ9V-ivW8Lv0cDq60tqf9HMM5zJdt9mxLTdOklyApN-9-UyVP_D-e5KfqM2aDXyYXYx",
 	},
 });
+
+
